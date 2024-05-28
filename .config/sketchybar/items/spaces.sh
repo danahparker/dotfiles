9684553e44ccb80b881847d5da067d8f1553f65d
Dana Parker <danapar@amazon.com>
2024-05-28 16:09:49 -0700

    small tweaks to sketchybar

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -8,14 +8,14 @@ do
              --set space.$sid space=$sid                                 \
                               icon=$sid                                  \
                               label.font="sketchybar-app-font:Regular:16.0" \
-                              label.padding_right=20                     \
+                              label.padding_right=15                     \
                               label.y_offset=-1                          \
                               script="$PLUGIN_DIR/space.sh"              
 done
 
 sketchybar --add item space_separator left                             \
-           --set space_separator icon="􀆊"                                \
-                                 icon.color=$ACCENT_COLOR \
+           --set space_separator icon="􀆊"                              \
+                                 icon.color=$ACCENT_COLOR              \
                                  icon.padding_left=4                   \
                                  label.drawing=off                     \
                                  background.drawing=off                \
