58b2711ac6138619a12cd86a7e532e185145e5b5
Dana Parker <danapar@amazon.com>
2024-11-25 15:57:52 -0800

    add oil.nvim plugin

---
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+        },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+}
