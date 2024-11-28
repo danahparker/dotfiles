8dfb1b22025b1f5ac725b45b4728d32a1f1cabbe
Dana Parker <danapar@amazon.com>
2024-11-27 18:32:58 -0800

    add org mode plugin to nvim

---
+++ .config/nvim/lua/dana/plugins/orgmode.lua
@@ -0,0 +1,17 @@
+-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+-- add ~org~ to ignore_install
+-- require('nvim-treesitter.configs').setup({
+--   ensure_installed = 'all',
+--   ignore_install = { 'org' },
+-- })
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    require('orgmode').setup {
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+    }
+  end,
+}
