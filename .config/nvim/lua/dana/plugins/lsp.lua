be4bb021d22faf7133d0baa08c0a60e6c6d26391
Dana Parker <danapar@amazon.com>
2025-05-07 15:33:56 -0700

    Fix TypeScript LSP

--- .config/nvim/lua/dana/plugins/lsp.lua
+++ .config/nvim/lua/dana/plugins/lsp.lua
@@ -2,13 +2,13 @@ return {
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      -- manual lsp setup
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
+    lazy = false,
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/nvim-cmp',
+      'L3MON4D3/LuaSnip',
+    }
   },
   {
     'williamboman/mason.nvim',
@@ -18,8 +18,13 @@ return {
     },
     config = true,
   },
-
-  -- snippets
+  {
+    'williamboman/mason-lspconfig.nvim',
+    lazy = false,
+    opts = {
+      ensure_installed = {'ts_ls'}
+    },
+  },
   {
     'L3MON4D3/LuaSnip',
     dependencies = {
@@ -27,13 +32,14 @@ return {
       'rafamadriz/friendly-snippets',
     },
   },
-
-  -- auto-completion
   {
     'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
     dependencies = {
-      { 'L3MON4D3/LuaSnip' },
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
       local lsp_zero = require 'lsp-zero'
@@ -44,14 +50,28 @@ return {
       require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup {
-        formatting = lsp_zero.cmp_format { details = true },
+        formatting = {
+          fields = {'abbr', 'kind', 'menu'},
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
         },
         mapping = cmp.mapping.preset.insert {
           ['<CR>'] = cmp.mapping.confirm { select = false },
           ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
           ['<C-f>'] = cmp_action.luasnip_jump_forward(),
@@ -65,27 +85,13 @@ return {
       }
     end,
   },
-
-  -- LSP
   {
     'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason-lspconfig.nvim' },
-    },
-    --- if you want to know more about lsp-zero and mason.nvim
-    --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
     config = function()
-      local lsp_zero = require 'lsp-zero'
-      lsp_zero.extend_lspconfig()
-
+      local lsp_zero = require('lsp-zero')
+      local lspconfig = require('lspconfig')
       lsp_zero.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
-        -- turn off autoformat
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
         -- git keymaps
         vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
@@ -98,28 +104,13 @@ return {
         vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
       end)
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          -- default handler
-          function(server_name)
-            local opts = { buffer = bufnr, remap = false }
-            require('lspconfig')[server_name].setup {}
-          end,
-          -- lua handler
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-          -- typescript handler
-          ts_ls = function()
-            require('lspconfig').tsserver.setup {
-              filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'typescript.tsx' },
-              cmd = { 'typescript-language-server', '--stdio' },
-            }
-          end,
-        },
-      }
+      lspconfig.ts_ls.setup({
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        on_attach = function(client, bufnr)
+          print("TypeScript LSP attached!")
+          lsp_zero.on_attach(client, bufnr)
+        end,
+      })
     end,
   },
 }
