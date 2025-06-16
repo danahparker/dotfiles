88c249403ca5bce67ce701b834cf091e6222462c
Dana Parker <danapar@amazon.com>
2025-06-16 09:22:18 -0700

    nvim: turn off file formatting, fix diagnostics keybinds

--- .config/nvim/lua/dana/plugins/lsp.lua
+++ .config/nvim/lua/dana/plugins/lsp.lua
@@ -95,14 +95,17 @@ return {
       local lspconfig = require('lspconfig')
       lsp_zero.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
+        -- turn off file formatting
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
         -- git keymaps
         vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
         vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
@@ -110,7 +113,6 @@ return {
       lspconfig.ts_ls.setup({
         capabilities = require('cmp_nvim_lsp').default_capabilities(),
         on_attach = function(client, bufnr)
-          print("TypeScript LSP attached!")
           lsp_zero.on_attach(client, bufnr)
         end,
       })
