b11bb7225afd496bf0f64fac87e8446b30875977
Dana Parker <danapar@amazon.com>
2025-07-03 09:11:11 -0700

    fix(nvim): fix obsidian plugin config

--- .config/nvim/lua/dana/plugins/obsidian.lua
+++ .config/nvim/lua/dana/plugins/obsidian.lua
@@ -1,28 +1,26 @@
+vim.o.conceallevel = 1
 return {
   'epwalsh/obsidian.nvim',
   version = '*',
   lazy = true,
-  -- ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  event = {
-    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    -- refer to `:h file-pattern` for more examples
-    'BufReadPre path/to/my-vault/*.md',
-    'BufNewFile path/to/my-vault/*.md',
-  },
+  ft = "markdown",
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
   opts = {
+    daily_notes = {
+      folder = "daily",
+      date_format = "%Y-%m-%d",
+      template = "templates/daily.md"
+    },
     workspaces = {
       {
         name = 'personal',
-        path = '~/Documents/obsidian/Personal',
+        path = '~/obsidian/Personal',
       },
       {
         name = 'amazon',
-        path = '~/Documents/obsidian/Amazon',
+        path = '~/obsidian/Amazon',
       },
     },
   },
