046d43f1fe67a40352e6461917d46f86b794d632
Dana Parker <danapar@amazon.com>
2025-07-10 14:26:55 -0700

    feat(nvim): add templates folder to obsidian plugin

--- .config/nvim/lua/dana/plugins/obsidian.lua
+++ .config/nvim/lua/dana/plugins/obsidian.lua
@@ -13,6 +13,11 @@ return {
       date_format = "%Y-%m-%d",
       template = "templates/daily.md"
     },
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
     workspaces = {
       {
         name = 'personal',
