1f8bffc88d8d97de3da12b040c6add37f6427acb
Dana Parker <danapar@amazon.com>
2025-10-06 15:04:20 -0700

    nvim: change telescope theme to ivy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,6 +46,7 @@ vim.wo.wrap = false
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {})
+vim.keymap.set('n', '<leader>p', ':silent %!prettier --stdin-filepath %<CR>', { silent = true })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {})
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {})
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {})
@@ -168,22 +169,14 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      {
         'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
         build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
         cond = function()
           return vim.fn.executable 'make' == 1
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
@@ -191,6 +184,12 @@ require('lazy').setup({
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
+          },
+          fzf = {}
+        },
+        pickers = {
+          find_files = {
+            theme = "ivy",
           },
         },
       }
